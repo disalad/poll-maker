949eb2a75156d3841d0153a1c95fc32d2503b758
disaladamsas <damsasdisala@gmail.com>
2022-05-26 22:34:26 +0530

    Improve styles and validation in log in page

--- auth/login.php
+++ auth/login.php
@@ -8,7 +8,8 @@ not_authed();
 
 $message = $username = $password = null;
 
-function login_user($connection, $username, $password) {
+function login_user($connection, $username, $password)
+{
     // Fetch data from the database
     $query = "SELECT * FROM users WHERE username = '$username'";
     $result = mysqli_fetch_assoc($connection->query($query));
@@ -49,22 +50,31 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log In</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Heisenberge Polls | Log In</title>
 </head>
 
-<body style="font-family:'Segoe UI'">
-    <h2>PHP AUTH</h2>
-
-    <h3><?php echo $message ?></h3>
-
-    <form action="<?php echo "/auth/login.php" ?>" method="POST">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" value="<?php echo $username ?>"><br><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br><br>
-        <input type="submit" value="Submit" name="submit">
-    </form>
-    <h3>Don't have an account? <a href="<?php echo "/auth/register.php" ?>">Register</a></h3>
+<body>
+    <div class="container">
+        <h3 class="mt-5 mb-4">Heisenberge Polls</h3>
+        <h6 class="mt-3 mb-4"><?php echo $message ?></h6>
+        <form action="/auth/login.php" method="POST" class="row g-3 mb-4">
+            <!-- Username -->
+            <div class="col-md-12">
+                <label class="form-label h6" for="inputUsername">Username</label>
+                <input type="text" class="form-control" id="inputUsername" name="username" placeholder="Username" value="<?php echo $username ?>">
+            </div>
+            <!-- Password -->
+            <div class="col-md-12">
+                <label for="inputPassword4" class="form-label h6">Password</label>
+                <input type="password" class="form-control" id="inputPassword4" name="password" placeholder="xxxxxx">
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary" name="submit">Register</button>
+            </div>
+        </form>
+        <h4>Don't have an account? <a href="<?php echo "/auth/register.php" ?>">Register</a></h4>
+    </div>
 </body>
 
 </html>
